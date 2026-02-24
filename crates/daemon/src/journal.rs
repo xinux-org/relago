05be3a4f3a406414718bdd0da0fb8ecb39e4d3f6
lambdajon <lambdajon42@gmail.com>
2026-02-24 21:30:59 +0500

    wip: parse scope

--- crates/daemon/src/journal.rs
+++ crates/daemon/src/journal.rs
@@ -1,7 +1,6 @@
-#![warn(rust_2018_idioms)]
-
 //! Follow future journal log messages and print up to 100 of them.
 use anyhow::anyhow;
+use nom::{self, Parser, branch::alt};
 use systemd::journal::{self, Journal, JournalEntryField, JournalSeek};
 use tracing::error;
 
@@ -9,7 +8,7 @@ pub mod fields {
     pub const MESSAGE: &str = "MESSAGE";
     pub const MESSAGE_ID: &str = "MESSAGE_ID";
     pub const PRIORITY: &str = "PRIORITY";
-    pub const UNIT: &str = "_SYSTEMD_UNIT";
+    pub const SYSTEMD_UNIT: &str = "_SYSTEMD_UNIT";
     pub const PID: &str = "_PID";
     pub const UID: &str = "_UID";
     pub const EXE: &str = "_EXE";
@@ -20,6 +19,14 @@ pub mod fields {
     pub const ERRNO: &str = "ERRNO";
     pub const CODE_FILE: &str = "CODE_FILE";
     pub const SYSLOG_IDENTIFIER: &str = "SYSLOG_IDENTIFIER";
+
+    // for service
+    pub const UNIT: &str = "UNIT";
+    pub const USER_UNIT: &str = "__SEQNUM"; // Fixme: remove
+
+    pub const JOB_RESULT: &str = "JOB_RESULT";
+    pub const EXIT_CODE: &str = "EXIT_CODE";
+    pub const EXIT_STATUS: &str = "EXIT_STATUS";
 
     // Coredump-specific fields
     pub const COREDUMP_PID: &str = "COREDUMP_PID";
@@ -33,15 +40,32 @@ pub mod fields {
 }
 
 #[derive(Debug, Clone)]
+pub enum Scope {
+    Service {
+        unit: String,
+        user_unit: Option<String>,
+        job_result: Option<String>,
+        exit_code: Option<u32>,
+        exit_signal: Option<String>,
+    },
+    Coredump {
+        exe: String
+    },
+
+}
+
+#[derive(Debug, Clone)]
 pub struct JournalEntry {
     pub message: String,
-    pub priority: Option<u8>,
-    pub unit: Option<String>,
+    pub user_unit: Option<String>,
+    // pub priority: Option<u8>,
+    pub systemd_unit: Option<String>,
     pub pid: Option<u32>,
     pub exe: Option<String>,
     pub comm: Option<String>,
     pub transport: Option<String>,
     pub errno: Option<i32>,
+    pub scope: Option<Scope>,
 }
 
 pub fn get_field(journal: &mut Journal, field: &str) -> Option<String> {
@@ -52,21 +76,50 @@ pub fn get_field(journal: &mut Journal, field: &str) -> Option<String> {
         .map(|v| v.into_owned())
 }
 
+pub fn parse_scope(journal: &mut Journal) -> Option<Scope> {
+    let user_unit = get_field(journal, fields::USER_UNIT);
+    match get_field(journal, "UNIT") {
+        Some(unit) => Some(Scope::Service {
+            unit,
+            user_unit,
+            job_result: get_field(journal, fields::JOB_RESULT),
+            exit_code: get_field(journal, fields::PID).and_then(|s| s.parse().ok()),
+            exit_signal: get_field(journal, fields::EXIT_STATUS),
+        }),
+        None => None,
+    }
+}
+
+pub fn parse_dump(journal: &mut Journal) -> Option<Scope> {
+    let exe = get_field(journal, "COREDUMP_EXE")
+        .or_else(|| get_field(journal, "_EXE"));
+    match exe {
+        Some(exe) => Some(Scope::Coredump { exe }),
+        None => None
+    }
+}
+
 /// Extract a structured [`JournalEntry`] from the current journal position.
 pub fn extract_entry(journal: &mut Journal) -> Option<JournalEntry> {
     let message = get_field(journal, fields::MESSAGE)?;
+    let user_unit = get_field(journal, fields::USER_UNIT);
+    let scope = parse_scope(journal).or(parse_dump(journal));
 
     Some(JournalEntry {
         message,
-        priority: get_field(journal, fields::PRIORITY).and_then(|s| s.parse().ok()),
-        unit: get_field(journal, fields::UNIT),
+        // priority: get_field(journal, fields::PRIORITY).and_then(|s| s.parse().ok()),
+        user_unit,
+        systemd_unit: get_field(journal, fields::SYSTEMD_UNIT),
         pid: get_field(journal, fields::PID).and_then(|s| s.parse().ok()),
         exe: get_field(journal, fields::EXE),
         comm: get_field(journal, fields::COMM),
         transport: get_field(journal, fields::TRANSPORT),
         errno: get_field(journal, fields::ERRNO).and_then(|s| s.parse().ok()),
+        // scope: parse_scope(journal),
+        scope,
     })
 }
+
 pub struct JournalTail {
     journal: Journal,
 }
@@ -94,9 +147,7 @@ impl JournalTail {
             .map_err(|e| anyhow!("Failed to add match {field}={value}: {e}"))?;
         Ok(self)
     }
-
 }
-
 
 impl Iterator for JournalTail {
     type Item = JournalEntry;
