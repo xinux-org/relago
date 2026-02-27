f2722102846495a6cf4b28d4e3a8652ca2057c58
lambdajon <lambdajon42@gmail.com>
2026-02-27 08:49:46 +0500

    revert coredump id is static

--- crates/daemon/src/crash.rs
+++ crates/daemon/src/crash.rs
@@ -3,7 +3,7 @@ use crash_event::CrashEvent;
 use relago_journal_ext::JournalExt;
 
 #[derive(Debug, CrashEvent)]
-#[journal(filter(SYSLOG_IDENTIFIER = "systemd-coredump"))]
+#[journal(filter(MESSAGE_ID = "fc2e22bc6ee647b6b90729ab34a250b1"))]
 pub struct CoredumpCrash {
     #[journal(field = "COREDUMP_EXE", required)]
     pub exe: String,
