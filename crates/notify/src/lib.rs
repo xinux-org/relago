d6ad386d8fd0f849479fabbf974306cd584a85c1
xfeusw <khamrakulovkamron@gmail.com>
2026-03-14 06:51:18 +0500

    refactor: using struct instead of string

--- crates/notify/src/lib.rs
+++ crates/notify/src/lib.rs
@@ -1,8 +1,18 @@
 mod window;
-
+use notify_rust::Notification;
+use window::Modal;
 use std::{env, fs};
 
-pub fn modal(error: String) {
-    println!("{}", error);
+pub fn modal(error: Modal) -> Result<(), Box<dyn std::error::Error>> {
+    let error_string = serde_json::to_string(&error)?;
+
+    Notification::new()
+        .summary("Error")
+        .body(format!("{}", error_string).as_str())
+        .icon(&error.exe)
+        .show()?;
+
     window::open(error);
+
+    Ok(())
 }
