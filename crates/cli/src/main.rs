173be1648a3a69d5b8942723e8d7df5ed21e4379
xfeusw <khamrakulovkamron@gmail.com>
2026-03-16 10:47:03 +0500

    refactor: cli main is rolled back

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,19 +1,12 @@
 #![allow(unused_must_use)]
 
-use std::{process::exit, fs, env};
+use std::exit;
 
 use clap::Parser;
-use notify::modal;
-use notify::window::Modal;
+use cli::run;
 
 fn main() -> anyhow::Result<()> {
-    let error = Modal {
-        unit: "xyz.service".to_string(),
-        exe: "firefox".to_string(),
-        message: "yebat".to_string(),
-    };
-
-    modal(error);
+    run();
 
     Ok(())
 }
