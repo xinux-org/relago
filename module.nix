5b381fac286f3fbb2dfb6458a50f82ddb63feeba
xfeusw <khamrakulovkamron@gmail.com>
2026-04-03 17:17:14 +0500

    refactor: doxuya on module

--- module.nix
+++ module.nix
@@ -124,32 +124,71 @@ let
         StateDirectory = cfg.user;
         StateDirectoryMode = "0750";
 
-        StandardInput = "null";
-        StandardOutput = "journal";
-        StandardError = "journal";
-
-        # Restart = "always";
-
-        DevicePolicy = "closed";
-        KeyringMode = "private";
-        LockPersonality = "yes";
-        MemoryDenyWriteExecute = "yes";
-        NoNewPrivileges = "yes";
-        PrivateDevices = "yes";
-        PrivateTmp = "true";
-        ProtectClock = "yes";
-        ProtectControlGroups = "yes";
-        # ProtectHome = "read-only";
-        ProtectHostname = "yes";
-        ProtectKernelLogs = "yes";
-        ProtectKernelModules = "yes";
-        ProtectKernelTunables = "yes";
-        ProtectProc = "invisible";
-        ProtectSystem = "full";
-        RestrictNamespaces = "yes";
-        RestrictRealtime = "yes";
-        RestrictSUIDSGID = "yes";
+        CapabilityBoundingSet = [
+          "AF_NETLINK"
+          "AF_INET"
+          "AF_INET6"
+        ];
+        DeviceAllow = [ "/dev/stdin r" ];
+        DevicePolicy = "strict";
+        # IPAddressAllow = "localhost";
+        LockPersonality = true;
+        PrivateDevices = true;
+        PrivateTmp = true;
+        PrivateUsers = false;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "strict";
+        ReadOnlyPaths = [ "/" ];
+        RemoveIPC = true;
+        RestrictAddressFamilies = [
+          "AF_NETLINK"
+          "AF_INET"
+          "AF_INET6"
+          "AF_UNIX"
+        ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
         SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "@system-service"
+          "~@privileged"
+          "~@resources"
+          "@pkey"
+        ];
+        UMask = "0027";
+
+        # # StandardInput = "null";
+        # # StandardOutput = "journal";
+        # # StandardError = "journal";
+
+        # # Restart = "always";
+
+        # DevicePolicy = "closed";
+        # KeyringMode = "private";
+        # LockPersonality = "yes";
+        # MemoryDenyWriteExecute = "yes";
+        # NoNewPrivileges = "yes";
+        # PrivateDevices = "yes";
+        # PrivateTmp = "true";
+        # ProtectClock = "yes";
+        # ProtectControlGroups = "yes";
+        # # ProtectHome = "read-only";
+        # ProtectHostname = "yes";
+        # ProtectKernelLogs = "yes";
+        # ProtectKernelModules = "yes";
+        # ProtectKernelTunables = "yes";
+        # ProtectProc = "invisible";
+        # ProtectSystem = "full";
+        # RestrictNamespaces = "yes";
+        # RestrictRealtime = "yes";
+        # RestrictSUIDSGID = "yes";
+        # SystemCallArchitectures = "native";
       };
     };
   };
