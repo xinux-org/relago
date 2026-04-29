9adc1e5edcc64d7b4083d644c72c1da9db872522
Kamron Khamrakulov <68609265+xfeusw@users.noreply.github.com>
2026-04-29 20:08:06 +0500

    feat: add exchanging keys (#31)
    * chore: my changes
    
    * chore: everything works now
    
    * chore: code is enhanced
    
    * refactor: removed debugging server
    
    * chore: completed
    
    * chore: removed server.pub
    
    * chore: resolved comments
    
    * refactor: code is zo'r qilindi
    
    * refactor: completed
    
    * fix: more clean way
    
    ---------
    
    Co-authored-by: lambdajon <lambdajon42@gmail.com>

--- crates/utils/src/setup_key.rs
+++ crates/utils/src/setup_key.rs
@@ -1,3 +1,8 @@
+use std::{
+    fs::{self, File},
+    path::PathBuf,
+};
+
 use crate::config::CONFIG;
 use pgp::{
     composed::{
@@ -7,32 +12,34 @@ use pgp::{
     crypto::ecc_curve::ECCCurve,
 };
 use rand::thread_rng;
+use reqwest::blocking::{multipart, Client, Response};
+use zip::ZipArchive;
 
-pub fn init() {
-    let write_path = CONFIG.get().keys.to_str().unwrap();
+#[derive(Clone)]
+enum GpgKeyType {
+    Pub,
+    Priv,
+}
 
+pub fn init() -> anyhow::Result<()> {
     let secret_key = keygen(
+        KeyType::Ed25519,
+        KeyType::Ed25519,
         KeyType::ECDH(ECCCurve::Curve25519),
-        KeyType::ECDH(ECCCurve::Curve25519),
-        KeyType::ECDH(ECCCurve::Curve25519),
-        KeyType::ECDH(ECCCurve::Curve25519),
+        KeyType::Ed25519,
         "",
     )
     .expect("failed during keygen");
 
-    let mut priv_file = std::fs::File::create(format!("{}/key.priv", write_path))
-        .expect("failed to create 'example-key.priv'");
-    secret_key
-        .to_armored_writer(&mut priv_file, None.into())
-        .expect("failed to write to 'example-key.priv'");
+    let _is_pub_key_created = create_key(&secret_key, GpgKeyType::Pub);
+    let _is_priv_key_created = create_key(&secret_key, GpgKeyType::Priv);
 
-    let public_key = SignedPublicKey::from(secret_key.clone());
+    let server_key =
+        exchange_keys(get_key_path(GpgKeyType::Pub)).expect("Couldn't exchange keys with server");
 
-    let mut pub_file = std::fs::File::create(format!("{}/key.pub", write_path))
-        .expect("failed to create 'example-key.pub'");
-    public_key
-        .to_armored_writer(&mut pub_file, None.into())
-        .expect("failed to write to 'example-key.pub'");
+    let _is_server_key_saved = save_key(server_key)?;
+
+    Ok(())
 }
 
 fn keygen(
@@ -81,4 +88,89 @@ fn keygen(
         .expect("Generate plain key");
 
     Ok(signed)
+}
+
+fn exchange_keys(key: PathBuf) -> anyhow::Result<Response> {
+    let server_route = format!("{}/keys/exchange", CONFIG.get().server.clone());
+
+    let form = multipart::Form::new().file("publicKey", key)?;
+
+    let client = Client::new();
+
+    let res = client.post(server_route).multipart(form).send()?;
+
+    Ok(res)
+}
+
+fn create_key(secret_key: &SignedSecretKey, key_type: GpgKeyType) -> anyhow::Result<()> {
+    let _is_keys_dir_created = fs::create_dir_all(CONFIG.get().keys.clone())?;
+
+    let mut file = fs::File::create(get_key_path(key_type.clone()))?;
+
+    match key_type {
+        GpgKeyType::Priv => {
+            secret_key.to_armored_writer(&mut file, None.into())?;
+        }
+        GpgKeyType::Pub => {
+            let public_key = SignedPublicKey::from(secret_key.clone());
+            public_key.to_armored_writer(&mut file, None.into())?;
+        }
+    }
+
+    Ok(())
+}
+
+fn save_key(res: Response) -> anyhow::Result<()> {
+    // /var/lib/relago
+    let root = CONFIG.get().data_dir.clone();
+    let keys = CONFIG.get().keys.clone();
+    let zip = PathBuf::from(format!("{}/res.zip", &keys.display()));
+
+    extract_zip(res, &zip, &keys)
+        .and_then(|_| move_id_file(&root, &keys))
+        .and_then(|_| move_key_file(&keys))
+        .and_then(|_| Ok(fs::remove_file(&zip)?))
+}
+
+fn extract_zip(mut res: Response, zip: &PathBuf, keys: &PathBuf) -> anyhow::Result<()> {
+    let mut created_file = File::create(zip)?;
+
+    res.copy_to(&mut created_file)?;
+
+    let opened_file = File::open(zip)?;
+
+    let mut zip = ZipArchive::new(&opened_file)?;
+
+    let mut _extracted = ZipArchive::extract(&mut zip, &keys);
+
+    Ok(())
+}
+
+fn move_id_file(root: &PathBuf, keys: &PathBuf) -> anyhow::Result<()> {
+    let from = PathBuf::from(format!("{}/idfile", keys.display()));
+
+    let to = PathBuf::from(format!("{}/user", root.display()));
+
+    let _is_id_copied = fs::copy(from, to);
+
+    Ok(())
+}
+
+fn move_key_file(keys: &PathBuf) -> anyhow::Result<()> {
+    let from = PathBuf::from(format!("{}/public.asc", keys.display()));
+
+    let to = PathBuf::from(format!("{}/server.pub", keys.display()));
+
+    let _is_key_moved = fs::rename(&from, &to);
+
+    Ok(())
+}
+
+fn get_key_path(key: GpgKeyType) -> PathBuf {
+    let keys_path = CONFIG.get().keys.clone();
+
+    PathBuf::from(match key {
+        GpgKeyType::Pub => format!("{}/key.pub", keys_path.display()),
+        GpgKeyType::Priv => format!("{}/key", keys_path.display()),
+    })
 }
